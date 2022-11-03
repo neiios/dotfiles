a0dd9595361d3ab96737a7025932af08f6e13171
Igor <68248740+richard96292@users.noreply.github.com>
2022-11-03 21:09:57 +0200

    Install applications with a script

--- alis-install-dotfiles.sh
+++ alis-install-dotfiles.sh
@@ -16,12 +16,16 @@ function paruInstall() {
   paru -S "${@}" --noconfirm --needed
 }
 
+function flatpakInstall() {
+  flatpak install --user -y --noninteractive "${@}"
+}
+
 function dotfileInstall() {
   stow -v 2 "${@}"
 }
 
-# prerequisites
-pacmanInstall stow git wl-clipboard xclip libnewt glib2 btop
+# deps
+pacmanInstall stow git libnewt
 
 # just in case someone decides to copy it to the wrong directory
 [[ ! -d ~/.dotfiles ]] && git clone https://github.com/richard96292/dotfiles ~/.dotfiles && cd ~/.dotfiles && git pull && git submodule init && git submodule update && bash alis-install-dotfiles.sh && exit # long boi
@@ -29,31 +33,44 @@ pacmanInstall stow git wl-clipboard xclip libnewt glib2 btop
 # initialize the repo
 git pull && git submodule init && git submodule update
 
-# environment variables
+# generic packages
+pacmanInstall firefox firefox-ublock-origin thunderbird \
+  zsh btop fzf bat exa github-cli neofetch ripgrep tldr yt-dlp \
+  foot foot-terminfo tmux \
+  element-desktop \
+  easyeffects \
+  keepassxc \
+  gimp inkscape \
+  libreoffice-fresh libreoffice-fresh-lt libreoffice-fresh-ru \
+  qbittorrent \
+  simple-scan \
+  qpwgraph \
+  mpv zathura \
+  code \
+  nextcloud-client \
+  ansible ansible-lint sshpass python-argcomplete python-boto3 \
+  dolphin gwenview ark
+
+paruInstall code-features code-icons code-marketplace \
+  nsxiv
+
+flatpakInstall flathub com.github.tchx84.Flatseal \
+  com.discordapp.Discord \
+  com.github.Eloston.UngoogledChromium \
+  com.github.taiko2k.tauonmb org.musicbrainz.Picard \
+  com.obsproject.Studio org.kde.kdenlive \
+  org.telegram.desktop
+
 dotfileInstall env
-
-# git
-pacmanInstall github-cli
 dotfileInstall git
-
-# terminal
-pacmanInstall foot foot-terminfo xdg-utils libnotify
 dotfileInstall foot
-
-# tmux
-pacmanInstall tmux
 dotfileInstall tmux
-
-# hotkey
 dotfileInstall hotkey
-
-# zsh and cli stuff
-pacmanInstall zsh fzf bat exa
 dotfileInstall zsh
 [[ ! "$(readlink /proc/$$/exe)" == "/usr/bin/zsh" ]] && chsh -s /usr/bin/zsh || echo "zsh is already used"
 
 # neovim
-pacmanInstall neovim python-pynvim stylua cppcheck clang lua-language-server bash-language-server shellcheck shfmt typescript-language-server ansible-lint fd fzf
+pacmanInstall neovim python-pynvim stylua cppcheck clang yamllint lua-language-server bash-language-server shellcheck shfmt typescript-language-server ansible-lint fd fzf
 paruInstall prettierd vscode-langservers-extracted ansible-language-server 
 dotfileInstall nvim
 [[ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]] && git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -61,16 +78,25 @@ nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 # sway
 if (whiptail --title "Sway" --yesno "Should the sway window manager be installed and configured?" 0 0); then
-  pacmanInstall sway wlroots swaybg swayidle swaylock wf-recorder grim slurp mako xdg-desktop-portal-wlr polkit xorg-xwayland bluez-utils
-  paruInstall tofi polkit-dumb-agent
+  pacmanInstall sway wlroots xorg-xwayland swaybg swayidle swaylock \
+    xdg-desktop-portal-gtk xdg-desktop-portal-wlr \
+    wf-recorder grim slurp swappy \
+    pavucontrol bluez-tools \
+    udiskie \
+    mako
+  paruInstall tofi polkit-dumb-agent-git
   dotfileInstall sway tofi mako swappy fonts
 fi
-
-# mangohud
-# flatpak steam doesnt work if mangohud config is a symlink so just copy it manually
-mkdir -pv ~/.config/MangoHud && cp ~/.dotfiles/mangohud/.config/MangoHud/MangoHud.conf ~/.config/MangoHud
 
 # gtk
 dotfileInstall gtk
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+if (whiptail --title "Gaming" --yesno "Do you really?" 0 0); then
+  # mangohud
+  # flatpak steam doesnt work if mangohud config is a symlink so just copy it manually
+  mkdir -pv ~/.config/MangoHud && cp ~/.dotfiles/mangohud/.config/MangoHud/MangoHud.conf ~/.config/MangoHud
+  # programs
+  flatpakInstall flathub com.valvesoftware.Steam com.valvesoftware.Steam.CompatibilityTool.Proton-GE com.valvesoftware.Steam.Utility.gamescope org.freedesktop.Platform.VulkanLayer.MangoHud//22.08
+fi
