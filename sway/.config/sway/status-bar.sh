91163cc6d2f17bd808966bd139fcb439285233f6
Igor <68248740+richard96292@users.noreply.github.com>
2022-11-23 11:43:12 +0200

    Only show minutes in the statusbar

--- sway/.config/sway/status-bar.sh
+++ sway/.config/sway/status-bar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-currentTime=$(date "+%F %T")
+currentTime=$(date "+%F %H:%M")
 language=$(swaymsg -t get_inputs | jq 'map(select(has("xkb_active_layout_name")))[0].xkb_active_layout_name')
 
 echo "${currentTime} - ${language}"
