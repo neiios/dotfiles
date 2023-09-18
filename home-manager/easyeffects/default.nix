90d3a65d4c1c5827aa65e0f99a04ed3a06fb9ae4
Igor <egor@sgf.lt>
2023-09-18 12:49:54 +0300

    Update easyeffects presets

--- home-manager/easyeffects/default.nix
+++ home-manager/easyeffects/default.nix
@@ -6,6 +6,6 @@
 } @ args: {
   # Install as a system package or a flatpak
   systemd.user.tmpfiles.rules = [
-    "L+ /home/egor/.config/easyeffects - - - - /home/egor/Dev/dotfiles/home-manager/easyeffects/config"
+    "L+ ${config.xdg.configHome}/easyeffects - - - - /home/egor/Dev/dotfiles/home-manager/easyeffects/config"
   ];
 }
