8f796bf7280375f53e2afe19f14e02421557ebc3
Igor <egor@sgf.lt>
2024-02-24 15:46:56 +0000

    Update gnome settings

--- home-manager/modules/gnome.nix
+++ home-manager/modules/gnome.nix
@@ -13,12 +13,7 @@
   ];
 
   dconf.settings = with lib.hm.gvariant; {
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      mic-mute = [ "Num_Lock" ];
-    };
-
     "org/gnome/desktop/input-sources" = {
-      xkb-options = [ "caps:escape" ];
       sources = [
         (mkTuple [
           "xkb"
@@ -33,15 +28,12 @@
           "lt"
         ])
       ];
+      xkb-options = [ "caps:escape" ];
     };
 
-    "org/gnome/settings-daemon/plugins/power" = {
-      power-button-action = "interactive";
-      sleep-inactive-ac-type = "nothing";
-    };
-
-    "org/gnome/mutter" = {
-      workspaces-only-on-primary = true;
+    "org/gnome/desktop/interface" = {
+      clock-format = "24h";
+      toolkit-accessibility = false;
     };
 
     "org/gnome/desktop/peripherals/mouse" = {
@@ -50,25 +42,56 @@
 
     "org/gnome/desktop/peripherals/touchpad" = {
       tap-to-click = true;
+      speed = 0.4;
+    };
+
+    "org/gnome/desktop/search-providers" = {
+      disabled = [ "org.gnome.Software.desktop" ];
     };
 
     "org/gnome/desktop/session" = {
       idle-delay = mkUint32 0;
     };
 
-    "org/gnome/desktop/sound" = {
-      event-sounds = false;
-      theme-name = "__custom";
-    };
-
     "org/gnome/desktop/wm/keybindings" = {
       close = [ "<Super>q" ];
+      move-to-workspace-last = [ "<Shift><Super>e" ];
+      move-to-workspace-left = [ "<Shift><Super>a" ];
+      move-to-workspace-right = [ "<Shift><Super>d" ];
+      switch-to-workspace-last = [ "<Super>e" ];
+      switch-to-workspace-left = [ "<Super>a" ];
+      switch-to-workspace-right = [ "<Super>d" ];
       toggle-maximized = [ "<Super>w" ];
     };
 
     "org/gnome/mutter" = {
+      dynamic-workspaces = true;
       edge-tiling = true;
-      dynamic-workspaces = false;
+      workspaces-only-on-primary = true;
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      mic-mute = [ "Num_Lock" ];
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      idle-dim = false;
+      power-button-action = "interactive";
+      power-saver-profile-on-low-battery = true;
+      sleep-inactive-ac-type = "nothing";
+      sleep-inactive-battery-type = "nothing";
+    };
+
+    "org/gnome/shell/keybindings" = {
+      toggle-application-view = [ ];
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      clock-format = "24h";
+    };
+
+    "system/locale" = {
+      region = "en_IE.UTF-8";
     };
   };
 }
