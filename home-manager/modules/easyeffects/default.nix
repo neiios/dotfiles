3fb90169353c55fb9ca216f98a04f1b24ca026fc
Igor <egor@sgf.lt>
2024-03-19 19:31:13 +0000

    Remove easyeffects state files from git

--- home-manager/modules/easyeffects/default.nix
+++ home-manager/modules/easyeffects/default.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}@args:
+{ config, pkgs, ... }@args:
 let
   nixGL = import ../../nixGL.nix {
     inherit pkgs config;
@@ -13,8 +8,4 @@ in
 {
   services.easyeffects.enable = true;
   services.easyeffects.package = (nixGL pkgs.easyeffects);
-
-  systemd.user.tmpfiles.rules = [
-    "L+ ${config.xdg.configHome}/easyeffects - - - - ${config.home.homeDirectory}/Configuration/home-manager/modules/easyeffects/config"
-  ];
 }
