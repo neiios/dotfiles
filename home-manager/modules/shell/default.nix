854bd406a061cdfc53bb6658f351230bbaca458c
Igor <egor@sgf.lt>
2024-03-19 19:37:58 +0000

    Add tmux session plugin

--- home-manager/modules/shell/default.nix
+++ home-manager/modules/shell/default.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  username,
-  ...
-}:
+{ config, pkgs, ... }:
 {
   imports = [
     ./foot.nix
@@ -41,6 +35,7 @@
       nfc = "nix flake check";
 
       ls = "ls --color=auto -lah";
+      cd = "z";
 
       pops = "podman ps -a --format='table {{.ID}}	{{.Names}}	{{.Image}}	{{.Status}}	{{.RunningFor}}'";
       pocu = "podman compose -d";
@@ -51,17 +46,27 @@
     };
 
     envExtra = ''
-      export XDG_DATA_DIRS=$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/igor/.local/share/flatpak/exports/share
+      # TODO: this is just scuffed, fix it
+      export XDG_DATA_DIRS=$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:${config.home.homeDirectory}/.local/share/flatpak/exports/share
     '';
 
     initExtra = ''
+      autoload -U +X bashcompinit && bashcompinit
+
       source ${./completions.zsh}
       source ${./prompt.zsh}
 
       # Disable paste highlight
       zle_highlight=('paste:none')
+
+      # Add local nvim to path
+      path=('${config.home.homeDirectory}/.local/nvim/bin' $path)
+
+      path=($path '${config.xdg.dataHome}/JetBrains/Toolbox/scripts')
     '';
   };
+
+  programs.zoxide.enable = true;
 
   programs.tmux.extraConfig = ''
     set -g default-shell ${pkgs.zsh}/bin/zsh
--- home-manager/modules/shell/tmux.nix
+++ home-manager/modules/shell/tmux.nix
@@ -1,10 +1,21 @@
+{ pkgs, ... }:
+let
+  tmux-session-wizard = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-session-wizard";
+    rtpFilePath = "session-wizard.tmux";
+    version = "v1.2.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "27medkamal";
+      repo = "tmux-session-wizard";
+      rev = "e9aac710119308d8dc4088667fd477883ff375e4";
+      sha256 = "IfSgX02vXdpzyu1GRF1EvzVCqqOEiTjeXtl1EvNr7EI=";
+    };
+  };
+in
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
+  # Needed for tmux-session-wizard
+  programs.zoxide.enable = true;
+
   programs.tmux = {
     enable = true;
     terminal = "tmux-256color";
@@ -18,13 +29,15 @@
     historyLimit = 50000;
     keyMode = "vi";
     mouse = true;
-    plugins = with pkgs; [
-      tmuxPlugins.mode-indicator
-      tmuxPlugins.yank
+    plugins = with pkgs.tmuxPlugins; [
+      mode-indicator
+      yank
+      {
+        plugin = tmux-session-wizard;
+        extraConfig = "set -g @session-wizard 'W'";
+      }
     ];
     extraConfig = ''
-      # tmux messages are displayed indefinitelly
-      set -g display-time 0
       # helps with autoread inside vim
       set-option -g focus-events on
 
@@ -35,24 +48,32 @@
       set -g status-justify absolute-centre
       set-window-option -g window-status-current-format "#[fg=black,bg=brightblue] #I:#W "
       set -g pane-active-border-style fg=blue
-      set-window-option -g status-left ""
+      set-window-option -g status-left "#[fg=black,bg=brightgreen] #S "
       set-window-option -g status-right ""
+
+      # reload config
+      unbind r
+      bind r source-file ~/.config/tmux/tmux.conf
 
       # create splits
       bind-key v split-window -h -c "#{pane_current_path}"
       bind-key s split-window -v -c "#{pane_current_path}"
+
       # resize panes (press prefix, hold ctrl and press key multiple times)
       bind-key -r C-j resize-pane -D 5
       bind-key -r C-k resize-pane -U 5
       bind-key -r C-h resize-pane -L 5
       bind-key -r C-l resize-pane -R 5
+
       # navigate panes
       bind -r h select-pane -L
       bind -r j select-pane -D
       bind -r k select-pane -U
       bind -r l select-pane -R
+
       # block selection in copy mode
       bind-key -Tcopy-mode-vi 'v' send -X begin-selection
+
       # move windows
       bind-key -n C-S-Left swap-window -t -1\; select-window -t -1
       bind-key -n C-S-Right swap-window -t +1\; select-window -t +1
