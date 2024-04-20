fa121dea511080d85f06cd3d91f279066374ac59
Igor <igor@sgf.lt>
2024-04-20 22:01:01 +0100

    Update gnome settings

--- home-manager/modules/gnome.nix
+++ home-manager/modules/gnome.nix
@@ -20,7 +20,7 @@
 
     "org/gnome/desktop/interface" = {
       clock-format = "24h";
-      toolkit-accessibility = false;
+      enable-animations = false;
       document-font-name = "Inter 11";
       font-name = "Inter 11";
       monospace-font-name = "JetBrainsMonoNL Nerd Font 10";
@@ -34,25 +34,12 @@
       accel-profile = "flat";
     };
 
-    "org/gnome/desktop/session" = {
-      idle-delay = mkUint32 0;
+    "org/gnome/desktop/privacy" = {
+      report-technical-problems = false;
     };
 
-    "org/gnome/desktop/wm/keybindings" = {
-      close = [ "<Super>q" ];
-      toggle-maximized = [ "<Super>w" ];
-      toggle-fullscreen = [ "<Shift><Super>w" ]; # borderless for every window. nice.
-
-      move-to-workspace-last = [ "<Shift><Super>e" ];
-      move-to-workspace-left = [ "<Shift><Super>a" ];
-      move-to-workspace-right = [ "<Shift><Super>d" ];
-      switch-to-workspace-last = [ "<Super>e" ];
-      switch-to-workspace-left = [ "<Super>a" ];
-      switch-to-workspace-right = [ "<Super>d" ];
-    };
-
-    "org/gnome/shell/keybindings" = {
-      toggle-application-view = [ ];
+    "org/gnome/system/location" = {
+      enabled = false;
     };
 
     "org/gnome/mutter" = {
@@ -61,8 +48,13 @@
       workspaces-only-on-primary = true;
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      mic-mute = [ "Num_Lock" ];
+    "org/gnome/desktop/session" = {
+      idle-delay = mkUint32 0;
+    };
+
+    "org/gnome/desktop/sound" = {
+      event-sounds = false;
+      theme-name = "__custom";
     };
 
     "org/gnome/settings-daemon/plugins/power" = {
@@ -73,8 +65,55 @@
       sleep-inactive-battery-type = "nothing";
     };
 
-    "org/gtk/settings/file-chooser" = {
-      clock-format = "24h";
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      mic-mute = [ "Num_Lock" ];
+    };
+
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+      switch-to-application-5 = [ ];
+      switch-to-application-6 = [ ];
+      switch-to-application-7 = [ ];
+      switch-to-application-8 = [ ];
+      switch-to-application-9 = [ ];
+      toggle-application-view = [ "<Super>s" ];
+      toggle-quick-settings = [ ];
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Super>q" ];
+
+      move-to-workspace-last = [ "<Shift><Super>e" ];
+      move-to-workspace-left = [ "<Shift><Super>a" ];
+      move-to-workspace-right = [ "<Shift><Super>d" ];
+      switch-to-workspace-last = [ "<Super>e" ];
+      switch-to-workspace-left = [ "<Super>a" ];
+      switch-to-workspace-right = [ "<Super>d" ];
+
+      maximize = [ "<Super>w" ];
+      toggle-fullscreen = [ "<Shift><Super>w" ];
+
+      move-to-workspace-1 = [ "<Super><Shift>1" ];
+      move-to-workspace-2 = [ "<Super><Shift>2" ];
+      move-to-workspace-3 = [ "<Super><Shift>3" ];
+      move-to-workspace-4 = [ "<Super><Shift>4" ];
+      move-to-workspace-5 = [ "<Super><Shift>5" ];
+      move-to-workspace-6 = [ "<Super><Shift>6" ];
+      move-to-workspace-7 = [ "<Super><Shift>7" ];
+      move-to-workspace-8 = [ "<Super><Shift>8" ];
+      move-to-workspace-9 = [ "<Super><Shift>9" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      switch-to-workspace-6 = [ "<Super>6" ];
+      switch-to-workspace-7 = [ "<Super>7" ];
+      switch-to-workspace-8 = [ "<Super>8" ];
+      switch-to-workspace-9 = [ "<Super>9" ];
     };
   };
 }
