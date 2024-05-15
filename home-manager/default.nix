17c2a9987b5656e87bb897b29af6969b98e82660
Igor <igor@sgf.lt>
2024-05-15 21:51:30 +0100

    Use experimental nixGL pr

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,15 +1,12 @@
-{ config, pkgs, ... }@args:
-let
-  nixGL = import ./nixGL.nix {
-    inherit pkgs config;
-    inherit (args) nixGL;
-  };
-in
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}@args:
 {
   imports = [
     ./modules/git
-    ./modules/foot.nix
-    ./modules/gnome.nix
     ./modules/packages.nix
     ./modules/shell.nix
     ./modules/tmux.nix
@@ -20,6 +17,8 @@ in
   home.homeDirectory = "/home/igor";
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
+
+  nixGL.prefix = "${args.nixGL.packages.x86_64-linux.nixGLIntel}/bin/nixGLIntel";
 
   fonts.fontconfig.enable = true;
   # Flatpaks won't be able to use these
@@ -45,26 +44,33 @@ in
     in
     {
       enable = true;
-      package = (nixGL mpvPackage);
+      package = (config.lib.nixGL.wrap mpvPackage);
     };
 
   services.easyeffects = {
     enable = true;
-    package = (nixGL pkgs.easyeffects);
+    package = (config.lib.nixGL.wrap pkgs.easyeffects);
   };
+
+  programs.kitty.enable = true;
+  programs.kitty.package = (config.lib.nixGL.wrap pkgs.kitty);
+  programs.kitty.extraConfig = ''
+    window_padding_width 10
+    background_opacity 0.9
+    shell ${lib.getBin pkgs.fish}/bin/fish
+
+    font_family      JetBrainsMonoNL Nerd Font
+    bold_font        auto
+    italic_font      auto
+    bold_italic_font auto
+  '';
 
   services.syncthing.enable = true;
 
   targets.genericLinux.enable = true;
 
-  nix.package = pkgs.nix;
   nix.registry.nixpkgs.flake = args.nixpkgs;
   nix.registry.home-manager.flake = args.home-manager;
-
-  # Be careful with this, modules can hardcode old paths
-  nix.settings.use-xdg-base-directories = true;
-  # Annoying and kinda useless
-  nix.settings.warn-dirty = false;
 
   # TODO: doesn't work with the new cli
   nixpkgs.config.allowUnfree = true;
