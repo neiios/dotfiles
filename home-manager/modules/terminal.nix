9a7a93e541f78c545f2c3b8af81cdb6369d56530
Igor <igor@sgf.lt>
2024-05-24 17:10:46 +0100

    Use foot terminal

--- home-manager/modules/terminal.nix
+++ home-manager/modules/terminal.nix
@@ -1,30 +1,75 @@
+{ pkgs, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}@args:
-let
-  ptyxisWrapped = (config.lib.nixGL.wrap args.self.packages.${pkgs.system}.ptyxis);
-in
-{
-  home.packages = [ ptyxisWrapped ];
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "JetBrainsMono Nerd Font:size=10, monospace:size=10";
+        pad = "16x16 center";
+        initial-window-size-pixels = "1200x700";
+        locked-title = "yes";
+        dpi-aware = "no";
+      };
+      scrollback = {
+        lines = "100000";
+        multiplier = "5.0";
+      };
+      csd = {
+        color = "ff000000";
+        button-color = "ffb9b9b9";
+      };
+      cursor = {
+        style = "block";
+        blink = "no";
+        beam-thickness = "1.5";
+      };
+      mouse = {
+        hide-when-typing = "no";
+        alternate-scroll-mode = "yes";
+      };
+      cursor = {
+        color = "181818 56d8c9";
+      };
+      colors = {
+        alpha = "1";
+        background = "000000";
+        foreground = "dddddd";
+        regular0 = "000000";
+        regular1 = "ff5f59";
+        regular2 = "44bc44";
+        regular3 = "d0bc00";
+        regular4 = "2fafff";
+        regular5 = "feacd0";
+        regular6 = "00d3d0";
+        regular7 = "bfbfbf";
+        bright0 = "595959";
+        bright1 = "ff6b55";
+        bright2 = "70b900";
+        bright3 = "fec43f";
+        bright4 = "79a8ff";
+        bright5 = "b6a0ff";
+        bright6 = "6ae4b9";
+        bright7 = "dddddd";
+      };
+    };
+  };
 
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/spawn-term/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/spawn-foot/"
       ];
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/spawn-term" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/spawn-foot" = {
       binding = "<Super>Return";
-      command = "${lib.getBin ptyxisWrapped}/bin/ptyxis";
+      command = "${pkgs.foot}/bin/foot ${pkgs.tmux}/bin/tmux new-session -s default -A";
       name = "Launch Terminal";
     };
   };
 
   programs.tmux.extraConfig = ''
-    set-option -a terminal-features 'xterm-256color:RGB'
+    # True color support
+    set-option -sa terminal-features ',foot:RGB'
   '';
 }
